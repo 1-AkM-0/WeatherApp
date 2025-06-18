e4b9445c969bc1a5d08b5ede915d782e29cd1e7c
1-AkM-0 <thepe880@gmail.com>
2025-06-18 15:09:34 -0300

    Add weather infos on html

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 // https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/
 // [location]/[date1]/[date2]?key=YOUR_API_KEY
 
+let city = "farias brito";
+const API_KEY = "77KPVW6YEATQHYYDNAR4NMFBB";
 async function getData() {
   const response = await fetch(
-    "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Sao%20Paulo/today?unitGroup=metric&include=current&key=77KPVW6YEATQHYYDNAR4NMFBB&contentType=json",
+    `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}/today?unitGroup=metric&include=current&key=${API_KEY}&contentType=json`,
     { mode: "cors" }
   );
   const weatherData = await response.json();
@@ -13,10 +15,10 @@ async function getData() {
 async function processData() {
   weatherData = await getData();
   const resolvedAddress = weatherData.resolvedAddress;
-  const temperatureF = weatherData.currentConditions.temp;
-  const temperatureC = (5 / 9) * (temperatureF - 32);
-  const feelsLikeF = weatherData.currentConditions.feelslike;
-  const feelsLikeC = (5 / 9) * (feelsLikeF - 32);
+  const temperatureC = weatherData.currentConditions.temp;
+  const temperatureF = temperatureC * 1.8 + 32;
+  const feelsLikeC = weatherData.currentConditions.feelslike;
+  const feelsLikeF = feelsLikeC * 1.8 + 32;
   const dateTime = new Date();
   const conditions = weatherData.currentConditions.conditions;
 
@@ -33,7 +35,7 @@ async function processData() {
   return {
     resolvedAddress,
     temperatureC,
-    temperatureC,
+    temperatureF,
     feelsLikeC,
     feelsLikeF,
     dateTime,
@@ -41,4 +43,17 @@ async function processData() {
   };
 }
 
-teste = processData();
+async function displayInfo() {
+  const data = await processData();
+
+  const location = document.querySelector(".location");
+  const temperature = document.querySelector(".degree");
+  const feel = document.querySelector(".feelslike");
+  const conditions = document.querySelector(".condition ");
+  location.textContent = data.resolvedAddress;
+  temperature.textContent = data.temperatureC;
+  feel.textContent = data.feelsLikeC;
+  conditions.textContent = data.conditions;
+}
+
+displayInfo();
